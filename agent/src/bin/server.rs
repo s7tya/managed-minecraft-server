1927720faa15300898a3f02aa25a4f2861e1a71b
Shina <53410646+s7tya@users.noreply.github.com>
2024-06-22 15:01:09 +0900

    server で thread を使うように

--- agent/src/bin/server.rs
+++ agent/src/bin/server.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 use agent::minecraft::{
     packet::{
@@ -17,43 +20,48 @@ fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:25565")?;
 
     for stream in listener.incoming() {
-        let mut stream = stream?;
-        let handshake: Handshake = read_packet(&mut stream)?;
+        thread::spawn(|| handle_request(&mut stream?));
+    }
 
-        match handshake.next_status {
-            0x01 => {
-                let _status_request: StatusRequest = read_packet(&mut stream)?;
+    Ok(())
+}
 
-                let status_response = status_response::StatusResponse {
-                    version: Version {
-                        name: "Motd Only Server".to_string(),
-                        protocol: 765,
-                    },
-                    players: Players {
-                        max: 100,
-                        online: 1,
-                        sample: None,
-                    },
-                    description: RawJsonText::String("Hello from Rust!".to_string()),
-                    modinfo: None,
-                    favicon: None,
-                };
-                stream.write_packet(status_response)?;
+fn handle_request(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let handshake: Handshake = read_packet(stream)?;
 
-                let ping: Ping = read_packet(&mut stream)?;
-                stream.write_packet(ping)?;
-            }
-            0x02 => {
-                stream.write_packet(DisconnectLogin {
-                    reason: RawJsonText::String("Hello!".to_string()),
-                })?;
-            }
-            _ => {
-                return Err(anyhow::anyhow!(
-                    "Invalid next_state: {}",
-                    handshake.next_status
-                ))
-            }
+    match handshake.next_status {
+        0x01 => {
+            let _status_request: StatusRequest = read_packet(stream)?;
+
+            let status_response = status_response::StatusResponse {
+                version: Version {
+                    name: "Motd Only Server".to_string(),
+                    protocol: 765,
+                },
+                players: Players {
+                    max: 100,
+                    online: 1,
+                    sample: None,
+                },
+                description: RawJsonText::String("Hello from Rust!".to_string()),
+                modinfo: None,
+                favicon: None,
+            };
+            stream.write_packet(status_response)?;
+
+            let ping: Ping = read_packet(stream)?;
+            stream.write_packet(ping)?;
+        }
+        0x02 => {
+            stream.write_packet(DisconnectLogin {
+                reason: RawJsonText::String("Hello!".to_string()),
+            })?;
+        }
+        _ => {
+            return Err(anyhow::anyhow!(
+                "Invalid next_state: {}",
+                handshake.next_status
+            ))
         }
     }
 
